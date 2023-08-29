344fb395d21663c2133531bb808da8197b9f6d92
0319119 <professionalalexwilson@gmail.com>
2023-08-29 13:02:39 +0400

    pushing web on github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Route from './routes/route'
+
+function App() {
+  return (
+     <div>
+        <Route />
+     </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container border border-dark'>Home</div>
+  )
+}
+
+export default Home
