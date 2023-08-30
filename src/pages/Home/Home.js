e25f3f2a8db8aade0b10ad0418e0c89b19f6ee3a
Abdur Rehman <a.rehman.mblk@gmail.com>
2023-08-30 13:20:41 +0400

    change the folder name

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,25 +1,19 @@
 import React from 'react'
-<<<<<<< HEAD
 import Header from "../../components/Header"
-=======
 import SmartChoice from '../../components/SmartChoice'
 import DevopsCom from '../../components/DevopsCom'
 import Services from '../../components/Services'
->>>>>>> 8a458c5a10473697676de6ef1886ad459cd15e68
 
 function Home() {
   return (
     <>
-<<<<<<< HEAD
     <section>
         <Header />
     </section>
     
-=======
       <DevopsCom />
       <Services />
       <SmartChoice />
->>>>>>> 8a458c5a10473697676de6ef1886ad459cd15e68
     </>
   )
 }
