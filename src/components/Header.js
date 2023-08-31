44760f125523c655be2e242d8bf7bcac2cd6ad97
0319119 <professionalalexwilson@gmail.com>
2023-08-31 13:31:52 +0400

    code marge

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,6 @@ import menImg from '../assets/image/head-man.png'
 import boxBotton from '../assets/image/box-bottom.png'
 import shapes from '../assets/image/shape3.png'
 import StandImg from '../assets/image/new.png'
-import { CgMenuLeft as MenuIcon } from 'react-icons/cg'
-import ImgLogo from '../Assets/image/logo-mesh-final-logo.png'
-import box1 from '../Assets/image/box1-1.png'
-import metrix from '../Assets/image/metrix.png'
-import menImg from '../Assets/image/head-man.png'
-import boxBotton from '../Assets/image/box-bottom.png'
-import shapes from '../Assets/image/shape3.png'
-import StandImg from '../Assets/image/new.png'
 import Slider from "react-slick";
 // import { a } from 'react-router-dom'
 
@@ -33,43 +25,6 @@ function Header() {
 
   return (
     <>
-    {/* <div className='container-fluid' id={styles.Main_Container}>
-        <div className="container p-2">
-         <div className="row">
-            <div className="col-lg-12 d-flex justify-content-between">
-                <div className={styles.Header}>
-                          <img src={ImgLogo} alt="" className={styles.header_logo} />
-                </div>
-                      <div className='px-3 d-flex align-items-center justify-content-center'>
-                    <span className='mx-1'><PhoneICon className={styles.HeaderIcon} /></span>
-                          <span className={styles.Header_number}>(786) 305 7060</span>
-                    <span><MenuIcon className={styles.MenuIcon} /></span>
-                </div>
-            </div>
-         </div>
-        </div>
-          <Slider {...settings}>
-
-        <div className="container">
-            <div className='row'>
-                <div className="col-lg-12 d-flex justify-content-center">
-                    <div>
-                       <img src={box1} alt="" />
-                    </div>
-                     <div className='d-flex flex-column' id={styles.Cont}>
-                              <img src={menImg} alt="" className={styles.menhead} />   
-                    <img src={StandImg} className={styles.std_imgs} />
-                    <img src={metrix} alt="" className={styles.metrix} />
-                    </div> 
-                    <div className='d-flex  flex-column justify-content-between' id={styles.rightside}>
-                              <img src={shapes} className={styles.Shapes}/>
-                              <img src={boxBotton} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div>
-      </Slider>
-    </div> */}
     <div className='container-fluid' id={styles.Header_Contact_Container}>
         <div className="container">
           <div className="row">
@@ -101,7 +56,6 @@ function Header() {
         </div>
         <div className="container-fluid">
           <div className="row">
-<<<<<<< HEAD
             <div className="col-lg-12 col-md-12 col-sm-12" >
               <div className='d-flex justify-content-center' id={styles.social_Links}>
               <Link to="/" className={styles.Social_link}>FACEBOOK</Link>
@@ -111,34 +65,16 @@ function Header() {
               <Link to="/" className={styles.Social_link}>PINTEREST</Link>
               <Link to="/" className={styles.Social_link}>YOUTUBE</Link>
             </div>
-=======
-            <div className="col-lg-12">
-              <div className='p-4 d-flex  justify-content-between'>
-                <a className={styles.Social_link}>FACEBOOK</a>
-                <a className={styles.Social_link}>INSTAGRAM</a>
-                <a className={styles.Social_link}>TWITTER</a>
-                <a className={styles.Social_link}>LINKEDIN</a>
-                <a className={styles.Social_link}>PINTEREST</a>
-                <a className={styles.Social_link}>YOUTUBE</a>
-              </div>
->>>>>>> 2bcf490e497ee3cbd239155058d906dce93d9064
             </div>
           </div>
         </div>
         <div className="container-fluid">
           <div className="row">
             <div className="col-lg-12">
-<<<<<<< HEAD
               <div className='p-4 d-flex  justify-content-evenly' id={styles.social_Links}>
                 <Link to="/" className={styles.contact_link}>team@logomish.com</Link>
                 <Link to="/" className={styles.contact_link}>privacy policy</Link>
                 <Link to="/" className={styles.contact_link}>accessibility</Link>
-=======
-              <div className='p-4 d-flex  justify-content-evenly'>
-                <a className={styles.contact_link}>team@logomish.com</a>
-                <a className={styles.contact_link}>privacy policy</a>
-                <a className={styles.contact_link}>accessibility</a>
->>>>>>> 2bcf490e497ee3cbd239155058d906dce93d9064
               </div>
             </div>
           </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import Header from "../../components/Header"
 import SmartChoice from '../../components/SmartChoice'
 import DevopsCom from '../../components/DevopsCom'
@@ -10,18 +9,16 @@ import HomePort from '../../components/HomePort'
 import HomeSlider from '../../components/HomeSlider'
 import Footer from '../../components/Footer'
 import Packages from '../../components/packages/packages'
-import Header from "../../Components/Header"
-import SmartChoice from '../../Components/SmartChoice'
-import DevopsCom from '../../Components/DevopsCom'
-import Services from '../../Components/Services'
-import VideoCom from '../../Components/VideoCom'
-=======
-import DevopsCom from '../../Components/DevopsCom'
-import SmartChoice from '../../Components/SmartChoice'
-import Services from '../../Components/Services'
-import TheCohort from '../../Components/TheCohort'
-import ConnectourTeam from '../../Components/ConnectourTeam'
->>>>>>> 9b97b63f3e512d673a352d521fafd94bf3cd491c
+import Header from "../../components/Header"
+import SmartChoice from '../../components/SmartChoice'
+import DevopsCom from '../../components/DevopsCom'
+import Services from '../../components/Services'
+import VideoCom from '../../components/VideoCom'
+import DevopsCom from '../../components/DevopsCom'
+import SmartChoice from '../../components/SmartChoice'
+import Services from '../../components/Services'
+import TheCohort from '../../components/TheCohort'
+import ConnectourTeam from '../../components/ConnectourTeam'
 
 function Home() {
   return (
@@ -32,7 +29,6 @@ function Home() {
       <DevopsCom />
       <Services />
       <SmartChoice />
-<<<<<<< HEAD
       <DevopsServicesCom />
       <ClientSlider />
       <HomePort />
@@ -43,16 +39,10 @@ function Home() {
         <SmartChoice />
         <Packages />
         <Footer />
-=======
       <TheCohort />
       <ConnectourTeam/>
->>>>>>> 9b97b63f3e512d673a352d521fafd94bf3cd491c
     </>
   )
 }
 
-<<<<<<< HEAD
 export default Home;
-=======
-export default Home
->>>>>>> 9b97b63f3e512d673a352d521fafd94bf3cd491c
