25a3a5af3d830134f91a249dd989d5582ff39df6
Abdur Rehman <a.rehman.mblk@gmail.com>
2023-08-31 12:34:05 +0400

    pakages changes header footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import styles from '../assets/css/header.module.css'
 import { FiPhoneCall as PhoneICon} from 'react-icons/fi'
-import { CgMenuLeft as MenuIcon } from 'react-icons/cg'
+import { RxCross1 as MenuIcon } from 'react-icons/rx'
 import ImgLogo from '../assets/image/logo-mesh-final-logo.png'
 import box1 from '../assets/image/box1-1.png'
 import metrix from '../assets/image/metrix.png'
@@ -62,10 +62,10 @@ function Header() {
         </div>
       </Slider>
     </div> */}
-    <div className='container-fluid bg-dark'>
-        <div className="container p-2">
+    <div className='container-fluid' id={styles.Header_Contact_Container}>
+        <div className="container">
           <div className="row">
-            <div className="col-lg-12 d-flex justify-content-between p-3">
+            <div className="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-between p-3">
               <div className={styles.Header}>
                 <img src={ImgLogo} alt="" className={styles.header_logo} />
               </div>
@@ -93,22 +93,22 @@ function Header() {
         </div>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-lg-12">
-              <div className='p-4 d-flex  justify-content-between'>
-                <Link to="/" className={styles.Social_link}>FACEBOOK</Link>
-                <Link to="/" className={styles.Social_link}>INSTAGRAM</Link>
-                <Link to="/" className={styles.Social_link}>TWITTER</Link>
-                <Link to="/" className={styles.Social_link}>LINKEDIN</Link>
-                <Link to="/" className={styles.Social_link}>PINTEREST</Link>
-                <Link to="/" className={styles.Social_link}>YOUTUBE</Link>
-              </div>
+            <div className="col-lg-12 col-md-12 col-sm-12" >
+              <div className='d-flex justify-content-center' id={styles.social_Links}>
+              <Link to="/" className={styles.Social_link}>FACEBOOK</Link>
+              <Link to="/" className={styles.Social_link}>INSTAGRAM</Link>
+              <Link to="/" className={styles.Social_link}>TWITTER</Link>
+              <Link to="/" className={styles.Social_link}>LINKEDIN</Link>
+              <Link to="/" className={styles.Social_link}>PINTEREST</Link>
+              <Link to="/" className={styles.Social_link}>YOUTUBE</Link>
+            </div>
             </div>
           </div>
         </div>
         <div className="container-fluid">
           <div className="row">
             <div className="col-lg-12">
-              <div className='p-4 d-flex  justify-content-evenly'>
+              <div className='p-4 d-flex  justify-content-evenly' id={styles.social_Links}>
                 <Link to="/" className={styles.contact_link}>team@logomish.com</Link>
                 <Link to="/" className={styles.contact_link}>privacy policy</Link>
                 <Link to="/" className={styles.contact_link}>accessibility</Link>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,17 +3,19 @@ import Header from "../../components/Header"
 import SmartChoice from '../../components/SmartChoice'
 import DevopsCom from '../../components/DevopsCom'
 import Services from '../../components/Services'
+import Footer from '../../components/Footer'
+import Packages from '../../components/packages/packages'
 
 function Home() {
   return (
     <>
-    <section>
+      
         <Header />
-    </section>
-    
-      <DevopsCom />
-      <Services />
+        <DevopsCom />
+        <Services />
       <SmartChoice />
+        <Packages />
+        <Footer />
     </>
   )
 }
