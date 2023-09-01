cda0412167d85160ca70313ee55665ef84a8ed52
0319119 <professionalalexwilson@gmail.com>
2023-09-01 11:22:04 +0400

    Hamza Waseem

--- src/Components/HomeSlider.js
+++ src/Components/HomeSlider.js
@@ -13,6 +13,12 @@ import seven from '../pages/assest/images/sliders_images/glow-bottom.avi'
 // ===============================================================
 import scd_slide_one from '../pages/assest/images/sliders_images/logo1-1.avi'
 import scd_slide_two  from  '../pages/assest/images/sliders_images/women-2.avi'
+import scd_slide_three from '../pages/assest/images/sliders_images/logo6-1.avi'
+import scd_slide_four from '../pages/assest/images/sliders_images/lavender-1.avi'
+import scd_slide_five from '../pages/assest/images/sliders_images/butterfly1.avi'
+
+
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -46,7 +52,7 @@ export class SimpleSlider extends Component {
         dots: true,
         centerMode: true,
         arrows: false,
-        // className: "homeSlider",
+        className: "homeSlider",
         padding: false,
         centerPadding: "0",
         speed: 500,
@@ -61,34 +67,45 @@ export class SimpleSlider extends Component {
                     <div className="">
                         <img src={two} alt=""  className={`${style.one}`}/>
                         <img src={one} alt="" className={`${style.two}`}/>
+                        <img src={five} alt=""  className={`${style.five}`}/>
                         <img src={three} alt="" className={`${style.three}`}/>
                         <img src={seven} alt="" className={`${style.blinkerOne}`}/>
                         <img src={seven} alt="" className={`${style.blinkerTwo}`}/>
                         <img src={four} alt="" className={`${style.four}`}/>
                         <img src={seven} alt="" className={`${style.blinkerthree}`}/>
+                        <img src={six} alt=""  className={`${style.six}`}/>
                     </div>
-
-                    {/* 
-                    
-                    <img src={four} alt="" className={`${style.four}`}/>
-                    <img src={five} alt="" className={`${style.five}`}/>
-                    <img src={six} alt="" className={`${style.six}`}/> */}
                 </div>
-                {/* <div className={`${style.homeSLiderContent}`}>
-                    <h4 data-aos="fade-right">We are</h4>
-                    <span data-aos="fade-left">Devops services</span>
-                    <h5 data-aos="fade-left">Agency</h5>
+                <div className={`${style.contentBox1}`}>
+                  <div className={`${style.contentBox1FlexTagline}`}>
+                      <span className={`${style.tagLine}`}>We are</span>
+                  </div>
+                  <div className={`${style.contentBox1FlexTagline}`}>
+                    <h4 className={`${style.text}`}>Devops Services agency</h4>
+                  </div>
+                </div>
+            </div>
+
+            <div className={`${style.homeSliderWholeBox}`}>
+                <div className={`${style.homeSliderBox}`} id={`${style.scdSlide}`}>
+                    <div className={`${style.multipleImages}`}>
+                      <img src={scd_slide_four} alt="" className={`${style.four}`} />
+                      <img src={scd_slide_one} alt="" className={`${style.one}`} />
+                      <img src={scd_slide_three} alt="" className={`${style.three}`} />
+                      <img src={scd_slide_two} alt="" className={`${style.two}`} />
+                      <img src={scd_slide_five} alt="" className={`${style.five}`} />
+                    </div>
+                </div>
+                {/* <div className={`${style.contentBox1}`}>
+                  <div className={`${style.contentBox1FlexTagline}`}>
+                      <span className={`${style.tagLine}`}>We are</span>
+                  </div>
+                  <div className={`${style.contentBox1FlexTagline}`}>
+                    <h4 className={`${style.text}`}>Devops Services agency</h4>
+                  </div>
                 </div> */}
             </div>
 
-            {/* <div className={`${style.homeSliderWholeBox}`}>
-                <div className={`${style.homeSliderImageBox}`} id={`${style.scdSlide}`}>
-                </div>
-                <div className={`${style.homeSLiderContent}`}>
-                    <h4>crafting your</h4>
-                    <span>Digital dreams</span>
-                </div>
-            </div> */}
             <div>
               <h3>3</h3>
             </div>
